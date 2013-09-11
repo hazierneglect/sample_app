365c517aef5833f372495315bedf35062e650d1a
Charlie Getzen <hazier.neglect@gmail.com>
2013-09-10 21:30:19 -0700

    user-micropost

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
 
+  def home
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      #@feed_items = current_user.feed.paginate(page: params[:page])
+      
+      # Show all items, not just ones own.
+      @feed_items = Micropost.all.paginate(page: params[:page])
+    else
+      @feed_items = Micropost.all.paginate(page: params[:page])
+    end
+    
+  end
   def help
   end
   
