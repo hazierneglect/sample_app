43ce612a18c3fe7f954b7430ce059a0ef19c00bd
Charlie Getzen <hazier.neglect@gmail.com>
2013-09-10 22:58:34 -0700

    Half-assed search function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,18 @@ class StaticPagesController < ApplicationController
     if signed_in?
       @micropost  = current_user.microposts.build
       #@feed_items = current_user.feed.paginate(page: params[:page])
+    end
       
-      # Show all items, not just ones own.
-      @feed_items = Micropost.all.paginate(page: params[:page])
+    # Show all items, not just ones own.
+    
+    if params[:utf8] == '✓'
+      search= "\s*" + params[:q]
+      #render search.to_s
+      # =~ /Cats(.*)/
+      #@feed_items = Micropost.where("title = ?", search)
+      
+      @feed_items = Micropost.where("title  ?", 'REGEXP \[a-zA-Z0-9_]*Regexp.escape(search)\[a-zA-Z0-9_]*' )
+      #@feed_items = Micropost.where(CharIndex(search,"title"))
     else
       @feed_items = Micropost.all.paginate(page: params[:page])
     end
