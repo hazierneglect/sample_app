308aae84c805ae9d60efa8b01bdd52151804b691
Charlie Getzen <hazier.neglect@gmail.com>
2013-09-11 18:51:27 -0700

    come

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-#Bundler.require(:default, Rails.env)
+Bundler.require(:default, Rails.env)
 
 
 module SampleApp
@@ -25,7 +25,7 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-   
+    config.assets.initialize_on_precompile = false
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     
     #config.assets.initialize_on_precompile = false
